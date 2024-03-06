03554fd31b2cbe8dd1c2c528fa7ec255a018976e
Juan David Frank <jdfrank95@gmail.com>
2024-03-06 12:59:47 +0100

    Added documentation comments to RabbitMQ.Core.Handler.cs

--- RabbitMQ.Core/RabbitMQ.Core/Handler.cs
+++ RabbitMQ.Core/RabbitMQ.Core/Handler.cs
@@ -8,10 +8,21 @@ using System.Threading.Tasks;
 
 namespace RabbitMQ.Core
 {
+    /// <summary>
+    /// Represents a Handler to wrap the received messages inside of a list.
+    /// </summary>
     public class Handler
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public Handler() { }
 
+        /// <summary>
+        /// Wraps the boy of a message inside of a list.
+        /// </summary>
+        /// <param name="body">Body of the message to wrap.</param>
+        /// <returns>A List with body as its only element</returns>
         public virtual List<object> HandleMessage(string body)
         {
             return new List<object>() { body };
