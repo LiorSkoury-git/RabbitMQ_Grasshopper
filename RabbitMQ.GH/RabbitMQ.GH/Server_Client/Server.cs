fc2dba86227496b50d088eb4fc706dd9b874a5b6
Juan David Frank <jdfrank95@gmail.com>
2024-03-09 16:08:40 +0100

    Added documentatoin to RabbitMQ.GH.Server_Client.Server.

--- RabbitMQ.GH/RabbitMQ.GH/Server_Client/Server.cs
+++ RabbitMQ.GH/RabbitMQ.GH/Server_Client/Server.cs
@@ -11,50 +11,111 @@ using RabbitMQ.GH.Properties;
 
 namespace RabbitMQ.GH.Server_Client
 {
+    /// <summary>
+    /// Represents a RabbitMQ Server inheriting from the GH_component class.
+    /// </summary>
     public class Server : GH_Component
     {
+        #region Fields
+
+        /// <summary>
+        /// Represents the state of the timer that tracks message consumption time.
+        /// </summary>
         private bool timerStarted;
+
+        /// <summary>
+        /// Represents the last received messages.
+        /// </summary>
         private List<object> lastReceivedMessage;
+
+        /// <summary>
+        /// Represents a TCP connection to a RabbitMQ broker.
+        /// </summary>
         private Connection connection;
+
+        /// <summary>
+        /// Represents a message handler.
+        /// </summary>
         private Handler handler;
+
+        /// <summary>
+        /// Represents a received message counter.
+        /// </summary>
         private int counter;
 
-        public Server()
-          : base("Server", "Server",
-            "A server rabbitMQ object",
-            "RabbitMQ", "Server-Client")
+        #endregion Fields
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor. Invokes the base class constructor and then extends it by adding the
+        /// counter, lastReceivedMessage, and timerStarted members.
+        /// </summary>
+        public Server(): base("Server", "Server", "A server rabbitMQ object", "RabbitMQ", "Server-Client")
         {
             counter = 0;
             lastReceivedMessage = new List<object>() { "Turn on the Run to start consuming" };
             timerStarted = false;
         }
 
+        #endregion Constructor
+
+        /// <summary>
+        /// Represents the exposure level of the component.
+        /// </summary>
+        /// /// <value>
+        /// The value of 4 sets the Consumer component to be displayed in the second section of the Grasshopper toolbar.
+        /// </value>
         public override GH_Exposure Exposure => (GH_Exposure)4;
 
+        #region Methods
 
+        /// <summary>
+        /// Initializes a RabbitMQ.Core Connection.
+        /// </summary>
+        /// <param name="host">Server host name.</param>
+        /// <param name="port">Server port number.</param>
+        /// <param name="vHost">Virtual host name.</param>
         private void setConnection(string host, int port, string vHost)
         {
+            // Handles null values of vHost.
             if (vHost == null) { vHost = "/"; }
+
             connection = new Connection();
+
+            // Sets up the connection with authentication.
             if (RhinoDoc.ActiveDoc.RuntimeData.ContainsKey("RabbitMQ_USER") && RhinoDoc.ActiveDoc.RuntimeData.ContainsKey("RabbitMQ_PASS"))
             {
                 connection.setFactory(host, port, (string)RhinoDoc.ActiveDoc.RuntimeData["RabbitMQ_USER"], (string)RhinoDoc.ActiveDoc.RuntimeData["RabbitMQ_PASS"], vHost);
             }
+            // Sets up the connection without authentication.
             else
             {
                 connection.setFactory(host, port, vHost);
             }
+
+            // Handles connection errors.
             bool connect = connection.CreateConnection();
             if (!connect) { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Error connecting to RabbitMQ"); }
         }
 
+        /// <summary>
+        /// Sets the queue-exchange bindings for the connection.
+        /// </summary>
+        /// <param name="queue">Queue name.</param>
+        /// <param name="exchange">Exchange name.</param>
+        /// <param name="exType">Exchange type.</param>
+        /// <param name="routingKeys">List of routing keys.</param>
         private void setConnectionDetails(string queue, string exchange, int exType, List<string> routingKeys)
         {
+            // Sets up the message queue.
             if (queue != "") { connection.setQueue(queue, Exclusive: false); }
             else { connection.setQueue(queue, Exclusive: true); }
 
+            // Sets up the message exchange.
             if (exchange != "" && exchange != null)
             {
+                // Sets up bindings if routing keys are provided.
                 connection.setExchange(exchange, exType);
                 if (routingKeys != null)
                 {
@@ -69,6 +130,7 @@ namespace RabbitMQ.GH.Server_Client
                         connection.setBinding(queue, exchange, routingKeys);
                     }
                 }
+                // Sets up bindings if routing keys are not provided.
                 else
                 {
                     routingKeys = new List<string>() { "" };
@@ -76,34 +138,56 @@ namespace RabbitMQ.GH.Server_Client
                 }
             }
 
+            // Creates a basic RabbitMQ consumer and subscribes the HandleReceivedMessage method to its Received event.
             var consumer = new EventingBasicConsumer(connection.channel);
             consumer.Received += HandleReceivedMessage;
 
+            // Updates lastReceivedMessages.
             lastReceivedMessage = new List<object>() { "Waiting for messages..." };
             connection.channel.BasicConsume(queue: connection.queues[0].QueueName, autoAck: false, consumer: consumer);
             timerStarted = true;
         }
+
+        /// <summary>
+        /// Handles a newly received message.
+        /// </summary>
+        /// <param name="sender">Sender of the message.</param>
+        /// <param name="ea">Deliver event arguments.</param>
         private void HandleMessageReceived(object sender, BasicDeliverEventArgs ea)
         {
             // Acknowledge the message
             connection.channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            // Gets the message body decoded as String.
             byte[] body = ea.Body.ToArray();
             var data = Encoding.UTF8.GetString(body);
+            // Handles the message's data.
             var result = handler.HandleMessage(data);
+            // Updates lastReceivedMessage.
             lastReceivedMessage = result;
             ExpireSolution(true);
         }
+
+
+        /// <summary>
+        /// Handles a newly received message.
+        /// </summary>
+        /// <param name="sender">Sender of the message.</param>
+        /// <param name="ea">Deliver event arguments.</param>
         private void HandleReceivedMessage(object sender, BasicDeliverEventArgs ea)
         {
             try
             {
                 RhinoApp.WriteLine("Handling message...");
+                // Acknowledges the message.
                 connection.channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                // Gets the message body decoded as String.
                 var body = Encoding.UTF8.GetString(ea.Body.ToArray());
                 var result = handler.HandleMessage(body);
+                // Encode as bytes the result of handling the received message, and send it as a response.
                 var message = Utilities.Convertor.ConvertListToJson(result);
                 var encodedMessage = Encoding.UTF8.GetBytes(message);
                 connection.channel.BasicPublish("", ea.BasicProperties.ReplyTo, null, encodedMessage);
+                // Updates state variables.
                 lastReceivedMessage = result;
                 counter++;
                 RhinoApp.WriteLine("Message handled successfully.");
@@ -111,6 +195,7 @@ namespace RabbitMQ.GH.Server_Client
             }
             catch (Exception ex)
             {
+                //Error handling.
                 RhinoApp.WriteLine($"Exception in HandleMessageReceived: {ex.Message}");
                 ExpireSolution(true);
             }
@@ -158,7 +243,10 @@ namespace RabbitMQ.GH.Server_Client
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            // Represents the state of the component. true if it is activated false otherwise.
             bool b = false;
+            // RabbitMQ state variables.
+
             string host = null;
             int port = 5672;
             string queue = "";
@@ -169,6 +257,7 @@ namespace RabbitMQ.GH.Server_Client
             handler = new Handler();
             string vHost = null;
 
+            // Get data from the component´s inputs
             DA.GetData("Run", ref b);
             DA.GetData("Host", ref host);
             DA.GetData("Port", ref port);
@@ -203,6 +292,8 @@ namespace RabbitMQ.GH.Server_Client
 
         }
 
+        #endregion Methods
+
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
@@ -223,5 +314,6 @@ namespace RabbitMQ.GH.Server_Client
         {
             get { return new Guid("4DAFDE88-3E3C-4095-AFDF-EACBC8267EA8"); }
         }
+        
     }
 }
