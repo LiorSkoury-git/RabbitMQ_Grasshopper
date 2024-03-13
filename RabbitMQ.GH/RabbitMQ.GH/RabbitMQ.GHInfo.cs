188765c5561023326952624e660d7d1cc4430a55
Juan David Frank <jdfrank95@gmail.com>
2024-03-13 16:20:06 +0100

    Added documentation to RabbitMQ.GHInfo.cs

--- RabbitMQ.GH/RabbitMQ.GH/RabbitMQ.GHInfo.cs
+++ RabbitMQ.GH/RabbitMQ.GH/RabbitMQ.GHInfo.cs
@@ -6,23 +6,28 @@ using System.Drawing;
 
 namespace RabbitMQ.GH
 {
+
+    /// <summary>
+    /// Represents the general information for a grasshopper assembly.
+    /// </summary>
     public class RabbitMQ_GHInfo : GH_AssemblyInfo
     {
         public override string Name => "RabbitMQ.GH";
 
-        //Return a 24x24 pixel bitmap to represent this GHA library.
+        // Return a 24x24 pixel bitmap to represent this GHA library.
         public override Bitmap Icon
         { get { return Resources.BASIC; } }
 
-        //Return a short string describing the purpose of this GHA library.
+        // Return a short string describing the purpose of this GHA library.
         public override string Description => "";
 
+        // Unique identifier for the assembly.
         public override Guid Id => new Guid("3fd45f85-857f-4bf3-b5c3-237d7332444b");
 
-        //Return a string identifying you or your company.
+        // Return a string identifying you or your company.
         public override string AuthorName => "Lior Skoury";
 
-        //Return a string representing your preferred contact details.
+        // Return a string representing your preferred contact details.
         public override string AuthorContact => "lior.skoury@icd.uni-stuttgart.de";
     }
 }
